389827600ef2df5ac958b8df9b88cc536f069113
Ridercogswell <ridercogswell@gmail.com>
2022-08-09 01:01:24 -0700

    header

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
 import React from 'react';
-import Nav from './components/NavbarMinimal'
+import { HeaderMiddle } from './components/HeaderMiddle'
 import { Home } from './components/Home';
 import { About } from './components/About'
+import { ContactIcons } from './components/Contact'
 import './assets/index.css';
 
 function App() {
   return (
     <>
-      <Nav />
+      <HeaderMiddle links={[
+        {
+          link: 'https://ridercogswell.github.io/tunedin/',
+          label: 'GitHub'
+        },
+        {
+          link: ''
+        }
+      ]} />
       <Home />
       <About />
+      <ContactIcons />
     </>
   );
 }
--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -1,132 +1,118 @@
-import {
-  createStyles,
-  Text,
-  Title,
-  SimpleGrid,
-  TextInput,
-  Textarea,
-  Button,
-  Group,
-  ActionIcon,
-} from '@mantine/core';
-import { IconBrandGithub, IconBrandYoutube, IconBrandInstagram } from '@tabler/icons';
-import { ContactIconsList } from '../ContactIcons/ContactIcons';
+import { createStyles, ThemeIcon, Text, SimpleGrid, Box, Stack } from '@mantine/core';
+import { IconSun, IconPhone, IconMapPin, IconAt } from '@tabler/icons';
 
-const useStyles = createStyles((theme) => ({
+type ContactIconVariant = 'white' | 'gradient';
+
+interface ContactIconStyles {
+  variant: ContactIconVariant;
+}
+
+const useStyles = createStyles((theme, { variant }: ContactIconStyles) => ({
   wrapper: {
-    minHeight: 400,
-    boxSizing: 'border-box',
-    backgroundImage: `linear-gradient(-60deg, ${theme.colors[theme.primaryColor][4]} 0%, ${
-      theme.colors[theme.primaryColor][7]
-    } 100%)`,
-    borderRadius: theme.radius.md,
-    padding: theme.spacing.xl * 2.5,
+    display: 'flex',
+    alignItems: 'center',
+    color: theme.white,
+  },
 
-    [`@media (max-width: ${theme.breakpoints.sm}px)`]: {
-      padding: theme.spacing.xl * 1.5,
-    },
+  icon: {
+    marginRight: theme.spacing.md,
+    backgroundImage:
+      variant === 'gradient'
+        ? `linear-gradient(135deg, ${theme.colors.red[4]} 0%, ${
+            theme.colors.red[6]
+          } 100%)`
+        : 'none',
+    backgroundColor: 'transparent',
   },
 
   title: {
-    fontFamily: `Greycliff CF, ${theme.fontFamily}`,
-    color: theme.white,
-    lineHeight: 1,
+    color: variant === 'gradient' ? theme.colors.gray[6] : theme.colors.red[0],
   },
 
   description: {
-    color: theme.colors[theme.primaryColor][0],
-    maxWidth: 300,
-
-    [`@media (max-width: ${theme.breakpoints.sm}px)`]: {
-      maxWidth: '100%',
-    },
-  },
-
-  form: {
-    backgroundColor: theme.white,
-    padding: theme.spacing.xl,
-    borderRadius: theme.radius.md,
-    boxShadow: theme.shadows.lg,
-  },
-
-  social: {
-    color: theme.white,
-
-    '&:hover': {
-      color: theme.colors[theme.primaryColor][1],
-    },
-  },
-
-  input: {
-    backgroundColor: theme.white,
-    borderColor: theme.colors.gray[4],
-    color: theme.black,
-
-    '&::placeholder': {
-      color: theme.colors.gray[5],
-    },
-  },
-
-  inputLabel: {
-    color: theme.black,
-  },
-
-  control: {
-    backgroundColor: theme.colors[theme.primaryColor][6],
+    color: variant === 'gradient' ? theme.black : theme.white,
   },
 }));
 
-const social = [IconBrandGithub, IconBrandYoutube, IconBrandInstagram];
+interface ContactIconProps extends Omit<React.ComponentPropsWithoutRef<'div'>, 'title'> {
+  icon: React.FC<any>;
+  title: React.ReactNode;
+  description: React.ReactNode;
+  variant?: ContactIconVariant;
+}
 
-export function ContactUs() {
-  const { classes } = useStyles();
-
-  const icons = social.map((Icon, index) => (
-    <ActionIcon key={index} size={28} className={classes.social} variant="transparent">
-      <Icon size={22} stroke={1.5} />
-    </ActionIcon>
-  ));
-
+function ContactIcon({
+  icon: Icon,
+  title,
+  description,
+  variant = 'gradient',
+  className,
+  ...others
+}: ContactIconProps) {
+  const { classes, cx } = useStyles({ variant });
   return (
-    <div className={classes.wrapper}>
-      <SimpleGrid cols={2} spacing={50} breakpoints={[{ maxWidth: 'sm', cols: 1 }]}>
-        <div>
-          <Title className={classes.title}>Contact us</Title>
-          <Text className={classes.description} mt="sm" mb={30}>
-            Leave your email and we will get back to you within 24 hours
-          </Text>
+    <div className={cx(classes.wrapper, className)} {...others}>
+      {variant === 'gradient' ? (
+        <ThemeIcon size={40} radius="md" className={classes.icon}>
+          <Icon size={24} />
+        </ThemeIcon>
+      ) : (
+        <Box mr="md">
+          <Icon size={24} />
+        </Box>
+      )}
 
-          <ContactIconsList variant="white" />
-
-          <Group mt="xl">{icons}</Group>
-        </div>
-        <div className={classes.form}>
-          <TextInput
-            label="Email"
-            placeholder="your@email.com"
-            required
-            classNames={{ input: classes.input, label: classes.inputLabel }}
-          />
-          <TextInput
-            label="Name"
-            placeholder="John Doe"
-            mt="md"
-            classNames={{ input: classes.input, label: classes.inputLabel }}
-          />
-          <Textarea
-            required
-            label="Your message"
-            placeholder="I want to order your goods"
-            minRows={4}
-            mt="md"
-            classNames={{ input: classes.input, label: classes.inputLabel }}
-          />
-
-          <Group position="right" mt="md">
-            <Button className={classes.control}>Send message</Button>
-          </Group>
-        </div>
-      </SimpleGrid>
+      <div>
+        <Text size="xs" className={classes.title}>
+          {title}
+        </Text>
+        <Text className={classes.description}>{description}</Text>
+      </div>
     </div>
+  );
+}
+
+interface ContactIconsListProps {
+  data?: ContactIconProps[];
+  variant?: ContactIconVariant;
+}
+
+const MOCKDATA = [
+  { title: 'Email', description: 'ridercogswell@gmail.com', icon: IconAt },
+  { title: 'Phone', description: '+1 (916) 743 0563', icon: IconPhone },
+  { title: 'Address', description: 'San Francisco, Bay Area', icon: IconMapPin },
+  { title: 'Working hours', description: '24/7', icon: IconSun },
+];
+
+export function ContactIconsList({ data = MOCKDATA, variant }: ContactIconsListProps) {
+  const items = data.map((item, index) => <ContactIcon key={index} variant={variant} {...item} />);
+  return <Stack>{items}</Stack>;
+}
+
+export function ContactIcons() {
+  return (
+    <SimpleGrid cols={2} breakpoints={[{ maxWidth: 755, cols: 1 }]}>
+      <Box
+        sx={(theme) => ({
+          padding: theme.spacing.xl,
+          borderRadius: theme.radius.md,
+          backgroundColor: theme.white,
+        })}
+      >
+        <ContactIconsList />
+      </Box>
+
+      <Box
+        sx={(theme) => ({
+          padding: theme.spacing.xl,
+          borderRadius: theme.radius.md,
+          backgroundImage: `linear-gradient(135deg, ${theme.colors.red[6]} 0%, ${
+            theme.colors[theme.primaryColor][4]
+          } 100%)`,
+        })}
+      >
+        <ContactIconsList variant="white" />
+      </Box>
+    </SimpleGrid>
   );
 }
