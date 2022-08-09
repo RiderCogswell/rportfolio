1686e9770b6db48d1a1cf741270c8b9ad89e14cb
Ridercogswell <ridercogswell@gmail.com>
2022-08-09 15:35:50 -0700

    formatting

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -6,7 +6,7 @@ export const Home = (): JSX.Element => {
           <div className='intro-to-me'>
             <h1 id='about' className='header'>Hi, I'm <span className='rider'>Rider</span></h1>
             <p className='intro'>
-              I'm a Full-Stack Developer with interests in Mobile Development, Machine Learning, AR/VR, and Blockchain. 
+              I'm a Full-Stack Software Engineer with interests in Mobile Development, Machine Learning, AR/VR, and Blockchain. 
             </p>
           </div>  
         </section>
