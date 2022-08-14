b37a1eaf63a25974370a2527a92a3030867815cb
Ridercogswell <ridercogswell@gmail.com>
2022-08-13 17:40:47 -0700

    homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { HeaderMiddle } from './components/HeaderMiddle';
-import { About } from './components/About';
+import { About } from './pages/About';
 import { ContactIcons } from './pages/Contact';
-import { Home } from './components/Home';
+import { Home } from './pages/Home';
 import { FooterSimple } from './components/Footer';
 import './assets/index.css';
 
