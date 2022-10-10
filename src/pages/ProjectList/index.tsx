7873ea179780b9c74c57bc0e19e008c32fd8f613
Ridercogswell <ridercogswell@gmail.com>
2022-10-09 22:55:45 -0700

    building out projects

--- src/pages/ProjectList/index.tsx
+++ src/pages/ProjectList/index.tsx
@@ -7,63 +7,62 @@ export const ProjectList = () => {
         {
             title: 'due',
             technologies: 'React, GraphQL, Mongoose, MongoDB, Express.js, Node.js',
-            description: 'Modern application to help everyone track their finances with a Non-Relational databsase and smart design.',
             image: 'due.png',
             link: 'https://due-ucd.herokuapp.com/'
         },
         {
+            title: 'Chatter',
+            technologies: 'TypeScript, React, Socket.io, Node.js, Inquirer',
+            image: 'chatter.png',
+            link: 'https://chatterrr.vercel.app/'
+        },
+        {
             title: 'cmnt',
             technologies: 'Node.js, Express.js, MySQL, Sequelize, Handlebars, Heroku',
-            description: 'Cmnt is a Node.js application that empowers anonymous users with a random topic that they can comment on, like, share, etc.',
             image: 'cmnt.png',
             link: 'https://cmnt-app.herokuapp.com/'
         },
         {
             title: 'loople',
             technologies: 'Vanilla JS, HTML, CSS, Foundation, Youtube API, The News API',
-            description: 'Loople is an interactive webpage that allows a user to search a topic and be given a response on the latest news, videos, and in the future, social media posts',
             image: 'loople.png',
             link: 'https://ridercogswell.github.io/tunedin/'
         },        
         {
             title: 'Deep Thoughts',
             technologies: 'GraphQL, React, Mongoose, MongoDB, Express.js, Node.js',
-            description: 'Social network platform allowing users, to comment, friend, post and react!',
             image: 'loople.png',
             link: ''
         },
         {
             title: 'Food Festival',
             technologies: 'Node.js, WebPack, HTML, CSS, PWA',
-            description: 'Website for local food festival where you can reserve tickets, check the schedule, and more!',
             image: 'loople.png',
             link: ''
         },
         {
             title: 'Shop-Shop',
             technologies: 'React, GraphQL, PWA, Mongoose, MongoDB, Express.js, Node.js, Stripe',
-            description: 'E-Commerce Application utilizing modern technologies.',
             image: 'loople.png',
             link: ''
         },
         {
             title: 'Shop-Shop',
             technologies: 'React, GraphQL, PWA, Mongoose, MongoDB, Express.js, Node.js, Stripe',
-            description: 'E-Commerce Application utilizing modern technologies.',
+
             image: 'loople.png',
             link: ''
         },
         {
             title: 'Shop-Shop',
             technologies: 'React, GraphQL, PWA, Mongoose, MongoDB, Express.js, Node.js, Stripe',
-            description: 'E-Commerce Application utilizing modern technologies.',
             image: 'loople.png',
             link: ''
         },
     ];
 
     return(
-        <SimpleGrid cols={4} spacing={'xl'} >
+        <SimpleGrid cols={3} style={{margin: 'space-around'}}>
             {projects.map((project) => (
                 <div onClick={() => window.location.replace(project.link)}  >
                     <Image 
