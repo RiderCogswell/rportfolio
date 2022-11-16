9a76b97262a2cad2a33c4749da65ceb4a1c783db
Ridercogswell <ridercogswell@gmail.com>
2022-11-16 15:42:54 -0800

    typewriter effect

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
+import { useTypewriter, Cursor } from 'react-simple-typewriter';
 
 export const Home = (): JSX.Element => {
-    return(
-        <section className='intro-design'>
-          <div className='intro-to-me'>
-            <h1 id='about' className='header'>Hi, I'm <span className='rider'>Rider</span></h1>
-            <p className='intro'>
-              I'm a Full-Stack Software Engineer with interests in Mobile Development, Machine Learning, AR/VR, and Blockchain. 
-            </p>
-          </div>  
-        </section>
-    );
+  const [text] = useTypewriter({
+    words: ['Web Development', 'Deep Learning', 'AR/VR', 'Blockchain', 'Golang + Rust', 'Functional Programming'],
+    loop: 0,
+    delaySpeed: 2000
+  })
+
+  return(
+      <section className='intro-design'>
+        <div className='intro-to-me'>
+          <h1 id='about' className='header'>Hi, I'm <span className='rider'>Rider</span></h1>
+          <p className='intro'>
+            I'm a Full-Stack Software Engineer with interests in <span>{text}</span><Cursor /> 
+          </p>
+        </div>  
+      </section>
+  );
 };
